9cfb5c311c64ea0bf4e14a7ba5b87af73a153af1
USER <user@USERs-MacBook-Pro.local>
2022-01-11 12:54:42 +0545

    Classname issue

--- src/pages/Header/Homepage.js
+++ src/pages/Header/Homepage.js
@@ -4,39 +4,41 @@ export default function Homescreen() {
   return (
     <div>
       {/* <!-- desktop menu start here --> */}
-      <header class="header-section">
-        <div class="header-top">
-          <div class="container">
-            <div class="row justify-content-center align-items-center">
-              <div class="col-lg-5 col-12">
-                <div class="logo py-2">
+      <header className="header-section">
+        <div className="header-top">
+          <div className="container">
+            <div className="row justify-content-center align-items-center">
+              <div className="col-lg-5 col-12">
+                <div className="logo py-2">
                   <a href="#">
                     <b>Brook Meadow farm</b>
                   </a>
                 </div>
               </div>
-              <div class="col-lg-7 col-12">
-                <div class="ht-left">
-                  <ul class="lab-ul d-flex flex-wrap justify-content-end">
-                    <li class="d-flex flex-wrap align-items-center">
-                      <div class="ht-add-thumb mr-2">
+              <div className="col-lg-7 col-12">
+                <div className="ht-left">
+                  <ul className="lab-ul d-flex flex-wrap justify-content-end">
+                    <li className="d-flex flex-wrap align-items-center">
+                      <div className="ht-add-thumb mr-2">
                         <img src="assets/images/header/01.png" alt="address" />
                       </div>
-                      <div class="ht-add-content">
+                      <div className="ht-add-content">
                         <span>1306 Longview Dr, Harrisburg</span>
-                        <span class="d-block text-bold">
+                        <span className="d-block text-bold">
                           PA 17111, United States
                         </span>
                       </div>
                     </li>
 
-                    <li class="d-flex flex-wrap align-items-center">
-                      <div class="ht-add-thumb mr-2">
+                    <li className="d-flex flex-wrap align-items-center">
+                      <div className="ht-add-thumb mr-2">
                         <img src="assets/images/header/03.png" alt="call" />
                       </div>
-                      <div class="ht-add-content">
+                      <div className="ht-add-content">
                         <span>Make Call </span>
-                        <span class="d-block text-bold">+1 717-982-2038</span>
+                        <span className="d-block text-bold">
+                          +1 717-982-2038
+                        </span>
                       </div>
                     </li>
                   </ul>
@@ -48,59 +50,59 @@ export default function Homescreen() {
       </header>
       {/* <!-- desktop menu end here --> */}
       {/* <!-- about section start here --> */}
-      <section class="about-section relative pt-20">
-        <div class="container">
-          <div class="row align-items-center mb-15">
-            <div class="col-lg-6 col-12">
-              <div class="about-thumb">
+      <section className="about-section relative pt-20">
+        <div className="container">
+          <div className="row align-items-center mb-15">
+            <div className="col-lg-6 col-12">
+              <div className="about-thumb">
                 <img src="assets/images/about/01.png" alt="about-thumb" />
               </div>
             </div>
-            <div class="col-lg-6 col-12">
-              <div class="about-wrapper p-0">
-                <div class="about-title">
+            <div className="col-lg-6 col-12">
+              <div className="about-wrapper p-0">
+                <div className="about-title">
                   <h2>
-                    <span class="d-lg-block ">
+                    <span className="d-lg-block ">
                       {" "}
                       <b>Welcome to Brookmeadow Farms</b>
                     </span>
                   </h2>
                   <p>Our Weekly Opening time</p>
                 </div>
-                <div class="about-content">
-                  <ul class="lab-ul list-group">
-                    <li class="list-group-item py-1 px-0 border-none">
-                      <i class="icofont-close-squared mr-2 color-theme"></i>
+                <div className="about-content">
+                  <ul className="lab-ul list-group">
+                    <li className="list-group-item py-1 px-0 border-none">
+                      <i className="icofont-close-squared mr-2 color-theme"></i>
                       <b>Monday: </b>
                       <span>Closed</span>
                     </li>
-                    <li class="list-group-item py-1 px-0 border-none">
-                      <i class="icofont-close-squared mr-2 color-theme"></i>
+                    <li className="list-group-item py-1 px-0 border-none">
+                      <i className="icofont-close-squared mr-2 color-theme"></i>
                       <b>Tuesday: </b>
                       <span>Closed</span>
                     </li>
-                    <li class="list-group-item py-1 px-0 border-none">
-                      <i class="icofont-tick-boxed mr-2 color-theme"></i>
+                    <li className="list-group-item py-1 px-0 border-none">
+                      <i className="icofont-tick-boxed mr-2 color-theme"></i>
                       <b>Wednesday: </b>
                       <span>9:00 AM - 5:00 PM</span>
                     </li>
-                    <li class="list-group-item py-1 px-0 border-none">
-                      <i class="icofont-tick-boxed mr-2 color-theme"></i>
+                    <li className="list-group-item py-1 px-0 border-none">
+                      <i className="icofont-tick-boxed mr-2 color-theme"></i>
                       <b>Thursday: </b>
                       <span>9:00 AM - 5:00 PM</span>
                     </li>
-                    <li class="list-group-item py-1 px-0 border-none">
-                      <i class="icofont-tick-boxed mr-2 color-theme"></i>
+                    <li className="list-group-item py-1 px-0 border-none">
+                      <i className="icofont-tick-boxed mr-2 color-theme"></i>
                       <b>Friday: </b>
                       <span>9:00 AM - 5:00 PM</span>
                     </li>
-                    <li class="list-group-item py-1 px-0 border-none">
-                      <i class="icofont-tick-boxed mr-2 color-theme"></i>
+                    <li className="list-group-item py-1 px-0 border-none">
+                      <i className="icofont-tick-boxed mr-2 color-theme"></i>
                       <b>Saturday: </b>
                       <span>9:00 AM - 5:00 PM</span>
                     </li>
-                    <li class="list-group-item py-1 px-0 border-none">
-                      <i class="icofont-tick-boxed mr-2 color-theme"></i>
+                    <li className="list-group-item py-1 px-0 border-none">
+                      <i className="icofont-tick-boxed mr-2 color-theme"></i>
                       <b>Sunday: </b>
                       <span>9:00 AM - 5:00 PM</span>
                     </li>
@@ -118,15 +120,15 @@ export default function Homescreen() {
 
       {/* <!-- footer section start here --> */}
       <footer>
-        <div class="footer-top relative padding-tb bg-ash relative">
-          <div class="shape-images">
+        <div className="footer-top relative padding-tb bg-ash relative">
+          <div className="shape-images">
             <img src="assets/images/shape-img/01.png" alt="shape-images" />
           </div>
-          <div class="container">
-            <div class="section-wrapper row justify-center ">
-              <div class="col-xl-3 col-md-2">
-                <div class="post-item">
-                  <div class="footer-logo">
+          <div className="container">
+            <div className="section-wrapper row justify-center ">
+              <div className="col-xl-3 col-md-2">
+                <div className="post-item">
+                  <div className="footer-logo">
                     <b>Brook Meadow farm</b>
                   </div>
                   <p>
@@ -140,35 +142,35 @@ export default function Homescreen() {
                   </p>
                 </div>
               </div>
-              <div class="col-xl-3 col-md-2">
-                <div class="post-item">
-                  <div class="post-title">
+              <div className="col-xl-3 col-md-2">
+                <div className="post-item">
+                  <div className="post-title">
                     <b>Keep In Touch</b>
                   </div>
-                  <ul class="lab-ul footer-location">
+                  <ul className="lab-ul footer-location">
                     <li>
-                      <div class="icon-part">
-                        <i class="icofont-phone"></i>
+                      <div className="icon-part">
+                        <i className="icofont-phone"></i>
                       </div>
-                      <div class="content-part">
+                      <div className="content-part">
                         <p>+1 717-982-2038</p>
                         <p>+1 717-982-2038</p>
                       </div>
                     </li>
                     <li>
-                      <div class="icon-part">
-                        <i class="icofont-wall-clock"></i>
+                      <div className="icon-part">
+                        <i className="icofont-wall-clock"></i>
                       </div>
-                      <div class="content-part">
+                      <div className="content-part">
                         <p>Wed - Sunday 09:00 - 5:00</p>
                         <p>(except public holidays)</p>
                       </div>
                     </li>
                     <li>
-                      <div class="icon-part">
-                        <i class="icofont-location-pin"></i>
+                      <div className="icon-part">
+                        <i className="icofont-location-pin"></i>
                       </div>
-                      <div class="content-part">
+                      <div className="content-part">
                         <p>
                           1306 Longview Dr, Harrisburg, <br />
                           PA 17111, United States

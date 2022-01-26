547c1ce9668d1c4eb642d2979787e9969e75b197
USER <user@USERs-MacBook-Pro.local>
2022-01-26 21:46:04 +0545

    Stripe Publishable Key Changed

--- src/components/stripe-button/stripe-button.jsx
+++ src/components/stripe-button/stripe-button.jsx
@@ -4,7 +4,8 @@ import StripeCheckout from "react-stripe-checkout";
 import { clearCart, toggleCartHidden } from "../../redux/cart/cart.action";
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = "pk_test_xzHx8YWdlidyI8OPnRRs2VC7001kKdGhg7";
+  const publishableKey =
+    "pk_live_51KGo7TH8tnYo1EYRK1XjQX2wCkD5YSIXJCMQ6azYcJLcaryDFm6zLGxWAZNiteZHI3GQ7BT4LUXGmQYTmQFFCzZ9002Rh0DdHV";
 
   const onToken = (token) => {
     console.log(token);
